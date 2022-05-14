f526325a555e7efc63ef7cc14e5c43fc8eaf03f9
happy <happy@HAPPY-11700>
2022-05-14 17:04:23 +0900

    DestroyServlet.javaを作成

---
+++ src/controllers/DestroyServlet.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    public DestroyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Task t = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            //削除
+            em.getTransaction().begin();
+            em.remove(t);//削除実行
+            em.getTransaction().commit();//削除のコミット
+            em.close();
+
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+    }
+   }
+}
